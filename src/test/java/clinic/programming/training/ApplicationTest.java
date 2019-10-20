2db6ab61eb6f6e1ac5e23ac347fb0862cf8227b9
DenysFilatov7 <DenysFilatov@i.ua>
2019-10-20 17:39:49 +0300

    Eclipse start

--- src/test/java/clinic/programming/training/ApplicationTest.java
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -1,9 +1,10 @@
 package clinic.programming.training;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class ApplicationTest {
     private Application app;
